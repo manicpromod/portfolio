ac45193489ee1a91efb8439799eb7f6d1bd8137e
pmanickam <pmanickam@incomm.com>
2018-11-05 16:30:35 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  public ngOnInit() {
+
+    $(".menu-toggle").click(function(e) {
+      console.log("test");
+      e.preventDefault();
+      $("#sidebar-wrapper").toggleClass("active");
+      $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+      $(this).toggleClass("active");
+    });
+
+  }
 
 }
